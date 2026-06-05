d85ad7ebef103353d7f163e770b8a459ca3c1bf6
Matt Hall <matthew349hall@hotmail.com>
2026-06-04 23:26:21 -0700

    mh/alias

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/cli.ts"],
+  format: ["esm"],
+  platform: "node",
+  clean: true,
+  outDir: "dist",
+  banner: {
+    js: "#!/usr/bin/env node"
+  }
+});
